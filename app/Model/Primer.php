ec5a0a77432cf683304f87faa4a1b7b18c1ae8e1
Carlos Peña <mycalesis@gmail.com>
2013-08-24 01:49:42 +0300

    some validations for primer creation

--- app/Model/Primer.php
+++ app/Model/Primer.php
@@ -1,7 +1,15 @@
 <?php
 class Primer extends AppModel {
 	public $validate = array(
-			'primer_name' => array('rule' => 'notEmpty')
+			'primer_name' => array('rule' => 'notEmpty', 'required' => true),
+            'forward_or_reverse' => array(
+                'allowedChoice' => array(
+                    'rule' => array('inList', array('F', 'R', 'f', 'r')),
+                    'message' => 'Enter either F or R'
+                )
+            ),
+			'primer_seq' => array('rule' => 'notempty'),
+			'annealing_temp' => array('rule' => 'numeric', 'message' => 'only numbers allowed')
 	);
 }
 ?>
